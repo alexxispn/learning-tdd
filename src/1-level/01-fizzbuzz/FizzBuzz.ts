0358f7384eb1e003427d000061113d215cebb366
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 14:49:45 +0100

    refactor(rule of three): use a more generic solution

--- src/1-level/01-fizzbuzz/FizzBuzz.ts
+++ src/1-level/01-fizzbuzz/FizzBuzz.ts
@@ -1,8 +1,6 @@
 export class FizzBuzz {
 
   convert(integer: number): string {
-    if (integer === 2) return "2"
-    if (integer === 4) return "4"
-    return "1";
+    return String(integer)
   }
 }
