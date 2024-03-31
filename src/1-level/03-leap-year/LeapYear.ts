9b58eb2890f6b80fa7d12cfa00e17de1bc4a4d15
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-01 00:16:57 +0100

    refactor: simplified solution extracting constants to be more declarative

--- src/1-level/03-leap-year/LeapYear.ts
+++ src/1-level/03-leap-year/LeapYear.ts
@@ -1,5 +1,8 @@
 export class LeapYear {
   static for(year: number): boolean {
-    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    const isMultipleOf4 = year % 4 === 0;
+    const isMultipleOf100 = year % 100 === 0;
+
+    return isMultipleOf4 && !isMultipleOf100
   }
 }
