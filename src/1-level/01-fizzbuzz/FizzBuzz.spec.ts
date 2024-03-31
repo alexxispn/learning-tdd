b287f294472f845bb795e6b92df39d9562eadb54
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 15:39:55 +0100

    refactor: remove dead code

--- src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
+++ src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
@@ -1,23 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { FizzBuzz } from "./FizzBuzz.js";
 
-// Input  ->    Output
-// 1      ->    "1"
-// 2      ->    "2"
-// 3      ->    "Fizz"
-// 4      ->    "4"
-// 5      ->    "Buzz"
-// 6      ->    "Fizz"
-// 7      ->    "7"
-// 8      ->    "8"
-// 9      ->    "Fizz"
-// 10     ->    "Buzz"
-// 11     ->    "11"
-// 12     ->    "Fizz"
-// 13     ->    "13"
-// 14     ->    "14"
-// 15     ->    "FizzBuzz"
-
 describe("FizzBuzz", () => {
   it.each([
     [1, "1"],
