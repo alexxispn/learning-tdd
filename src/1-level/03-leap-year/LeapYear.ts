8301189b008ae4d4e717146c49a16dea76d76476
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 23:44:36 +0100

    green(fake it): is false for year 1900

--- src/1-level/03-leap-year/LeapYear.ts
+++ src/1-level/03-leap-year/LeapYear.ts
@@ -1,6 +1,6 @@
 export class LeapYear {
   static for(year: number): boolean {
-    if (year % 4 !== 0 || year === 1800) return false
+    if (year % 4 !== 0 || year === 1800 || year === 1900) return false
     return true
   }
 }
