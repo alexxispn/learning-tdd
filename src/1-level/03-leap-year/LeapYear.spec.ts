d17b13ec08ca077888913535618de188438d3735
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 23:41:29 +0100

    refactor: group test by generic condition for not multiple of 4

--- src/1-level/03-leap-year/LeapYear.spec.ts
+++ src/1-level/03-leap-year/LeapYear.spec.ts
@@ -6,17 +6,18 @@ import { LeapYear } from "./LeapYear.js";
 // 1600 -> true (divisible by 400)
 // 1800 -> false (divisible by 4, divisible by 100, NOT divisible by 400)
 
-describe("LeapYear", ()=> {
+describe("LeapYear", () => {
   it("is true for year 1996", () => {
     expect(LeapYear.for(1996)).toBe(true);
   });
 
-  it.each([
-    3,
-    1997,
-    1999,
-
-  ])("is false for year %i", (year) => {
-    expect(LeapYear.for(year)).toBe(false)
+  describe("false when is not multiple of 4", () => {
+    it.each([
+      3,
+      1997,
+      1999
+    ])("year %i", (year) => {
+      expect(LeapYear.for(year)).toBe(false);
+    });
   })
-})
+});
