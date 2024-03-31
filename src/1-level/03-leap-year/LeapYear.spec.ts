e54540094e847a2b82458cd96adfe53b630140e3
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 22:54:07 +0100

    test: write a list of test cases

--- src/1-level/03-leap-year/LeapYear.spec.ts
+++ src/1-level/03-leap-year/LeapYear.spec.ts
@@ -1,5 +1,10 @@
 import { describe, expect, it } from "vitest";
 
+// 1997 -> false (not divisible by 4)
+// 1996 -> true (divisible by 4)
+// 1600 -> true (divisible by 400)
+// 1800 -> false (divisible by 4, divisible by 100, NOT divisible by 400)
+
 describe("LeapYear", ()=> {
   it("should return true", () => {
     expect(true).toBe(true)
