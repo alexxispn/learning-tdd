1450dd7900ed97c8abce866616c94f8f518d246c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 21:12:31 +0100

    refactor: remove dead code

--- src/1-level/02-manhattan-distance/ManhattanDistance.spec.ts
+++ src/1-level/02-manhattan-distance/ManhattanDistance.spec.ts
@@ -2,19 +2,6 @@ import { describe, expect, it } from "vitest";
 import { ManhattanDistance } from "./ManhattanDistance.js";
 import { Point } from "./Point.js";
 
-// {0, 0} -> {0, 0} = 0
-// {1, 1} -> {1, 1} = 0
-// {2, 2} -> {2, 2} = 0
-// {0, 0} -> {0, 1} = 1
-// {0, 0} -> {1, 0} = 1
-// {1, 1} -> {1, 0} = 1
-// {0, 0} -> {1, 1} = 2
-// {1, 1} -> {0, 0} = 2
-// {0, 0} -> {2, 2} = 4
-// {2, 2} -> {0, 0} = 4
-// {0, 0} -> {2, 3} = 5
-// {2, 3} -> {0, 0} = 5
-
 describe("ManhattanDistance", () => {
   describe("when the points are at the same location", () => {
     it.each([
