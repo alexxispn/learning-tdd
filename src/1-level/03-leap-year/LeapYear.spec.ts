7d8358c9eac698149ddac72ec6a29bc746fdf93b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-01 00:21:08 +0100

    refactor: remove dead code

--- src/1-level/03-leap-year/LeapYear.spec.ts
+++ src/1-level/03-leap-year/LeapYear.spec.ts
@@ -1,11 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { LeapYear } from "./LeapYear.js";
 
-// 1997 -> false (not divisible by 4)
-// 1996 -> true (divisible by 4)
-// 1600 -> true (divisible by 400)
-// 1800 -> false (divisible by 4, divisible by 100, NOT divisible by 400)
-
 describe("LeapYear", () => {
   describe("is true when is multiple of 4", () => {
     it.each([
