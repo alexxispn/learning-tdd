5e6b36ab4b416a7c7217e37caa2833c424bdac5a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-01 00:20:07 +0100

    green: is true when is multiple of 400

--- src/1-level/03-leap-year/LeapYear.ts
+++ src/1-level/03-leap-year/LeapYear.ts
@@ -2,7 +2,8 @@ export class LeapYear {
   static for(year: number): boolean {
     const isMultipleOf4 = year % 4 === 0;
     const isMultipleOf100 = year % 100 === 0;
+    const isMultipleOf400 = year % 400 === 0;
 
-    return isMultipleOf4 && !isMultipleOf100
+    return isMultipleOf4 && (isMultipleOf400 || !isMultipleOf100);
   }
 }
