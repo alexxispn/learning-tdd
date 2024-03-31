c6007662a327e6901e8944d259052c201ba656f8
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 21:12:17 +0100

    test(test parameterization): add more tests with parameterization

--- src/1-level/02-manhattan-distance/ManhattanDistance.spec.ts
+++ src/1-level/02-manhattan-distance/ManhattanDistance.spec.ts
@@ -16,21 +16,41 @@ import { Point } from "./Point.js";
 // {2, 3} -> {0, 0} = 5
 
 describe("ManhattanDistance", () => {
-  it("is 0 between two points at the same location", () => {
-    const origin = Point.at(0, 0);
-    const destination = Point.at(0, 0);
+  describe("when the points are at the same location", () => {
+    it.each([
+      [0, 0],
+      [1, 1],
+      [2, 2]
+    ])("is 0 between two points at the same location", (x, y) => {
+      const origin = Point.at(x, y);
+      const destination = Point.at(x, y);
 
-    const manhattanDistance = ManhattanDistance.between(origin, destination)
+      const manhattanDistance = ManhattanDistance.between(origin, destination);
 
-    expect(manhattanDistance).toBe(0);
+      expect(manhattanDistance).toBe(0);
+    });
   });
 
-  it("is 2 between two points at {1, 1} and {0, 0}", () => {
-    const origin = Point.at(0, 0);
-    const destination = Point.at(1, 1);
+  describe("when the points are on different locations", () => {
+    it.each([
+      { origin: { x: 0, y: 0 }, destination: { x: 0, y: 1 }, expected: 1 },
+      { origin: { x: 0, y: 0 }, destination: { x: 1, y: 0 }, expected: 1 },
+      { origin: { x: 1, y: 1 }, destination: { x: 1, y: 0 }, expected: 1 },
+      { origin: { x: 0, y: 0 }, destination: { x: 1, y: 1 }, expected: 2 },
+      { origin: { x: 1, y: 1 }, destination: { x: 0, y: 0 }, expected: 2 },
+      { origin: { x: 0, y: 0 }, destination: { x: 2, y: 2 }, expected: 4 },
+      { origin: { x: 2, y: 2 }, destination: { x: 0, y: 0 }, expected: 4 },
+      { origin: { x: 0, y: 0 }, destination: { x: 2, y: 3 }, expected: 5 },
+      { origin: { x: 2, y: 3 }, destination: { x: 0, y: 0 }, expected: 5 },
+      { origin: { x: 0, y: 0 }, destination: { x: 3, y: 2 }, expected: 5 },
+      { origin: { x: 3, y: 2 }, destination: { x: 0, y: 0 }, expected: 5 }
+    ])("is %i between two points on the same axis", ({ origin, destination, expected }) => {
+      const originPoint = Point.at(origin.x, origin.y);
+      const destinationPoint = Point.at(destination.x, destination.y);
 
-    const manhattanDistance = ManhattanDistance.between(origin, destination)
+      const manhattanDistance = ManhattanDistance.between(originPoint, destinationPoint);
 
-    expect(manhattanDistance).toBe(2);
+      expect(manhattanDistance).toBe(expected);
+    });
   });
-})
+});
