a96c5a19ce139438573aff7ce306b1ddf610d884
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 14:46:51 +0100

    refactor(test parameterization): simplified tests with parameterization

--- src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
+++ src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
@@ -19,27 +19,15 @@ import { FizzBuzz } from "./FizzBuzz.js";
 // 15     ->    "FizzBuzz"
 
 describe("FizzBuzz", () => {
-  it("convert 1 to string", () => {
+  it.each([
+    1,
+    2,
+    4
+  ])("convert %s to string", (input) => {
     const fizzBuzz = new FizzBuzz()
 
-    const converted = fizzBuzz.convert(1)
+    const converted = fizzBuzz.convert(input)
 
-    expect(converted).toBe("1")
-  })
-
-  it("convert 2 to string", () => {
-    const fizzBuzz = new FizzBuzz()
-
-    const converted = fizzBuzz.convert(2)
-
-    expect(converted).toBe("2")
-  })
-
-  it("convert 4 to string", () => {
-    const fizzBuzz = new FizzBuzz()
-
-    const converted = fizzBuzz.convert(4)
-
-    expect(converted).toBe("4")
+    expect(converted).toBe(input.toString())
   })
 })
