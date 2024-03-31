889ee6733fbe5985d0e0579b5e6d1cdfe1ed4d15
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 16:06:43 +0100

    refactor: group test by use cases

--- src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
+++ src/1-level/01-fizzbuzz/FizzBuzz.spec.ts
@@ -1,36 +1,44 @@
 import { describe, expect, it } from "vitest";
 import { FizzBuzz } from "./FizzBuzz.js";
 
-describe("FizzBuzz", () => {
-  it.each([
-    [1, "1"],
-    [2, "2"],
-    [4, "4"]
-  ])("convert %s to '%s'", (input, expected) => {
-    expect(FizzBuzz.convert(input)).toBe(expected)
+describe("FizzBuzz converts integers", () => {
+  describe("not multiples of 3 or 5 into string", () => {
+    it.each([
+      [1, "1"],
+      [2, "2"],
+      [4, "4"]
+    ])("%s to '%s'", (input, expected) => {
+      expect(FizzBuzz.convert(input)).toBe(expected)
+    })
   })
 
-  it.each([
-    3,
-    6,
-    9
-  ])("convert %s to 'Fizz'", (input) => {
-    expect(FizzBuzz.convert(input)).toBe("Fizz")
+  describe("multiples of 3 and not 5 into 'Fizz'", () => {
+    it.each([
+      3,
+      6,
+      9
+    ])("%s to 'Fizz'", (input) => {
+      expect(FizzBuzz.convert(input)).toBe("Fizz")
+    })
   })
 
-  it.each([
-    5,
-    10,
-    20
-  ])("convert %s to 'Buzz'", (input) => {
-    expect(FizzBuzz.convert(input)).toBe("Buzz")
+  describe("multiples of 5 and not 3 into 'Buzz'", () => {
+    it.each([
+      5,
+      10,
+      20
+    ])("%s to 'Buzz'", (input) => {
+      expect(FizzBuzz.convert(input)).toBe("Buzz")
+    })
   })
 
-  it.each([
-    15,
-    30,
-    45
-  ])("convert %s to 'FizzBuzz'", (input) => {
-    expect(FizzBuzz.convert(input)).toBe("FizzBuzz")
+  describe("multiples of 3 and 5 into 'FizzBuzz'", () => {
+    it.each([
+      15,
+      30,
+      45
+    ])("%s to 'FizzBuzz'", (input) => {
+      expect(FizzBuzz.convert(input)).toBe("FizzBuzz")
+    })
   })
 })
