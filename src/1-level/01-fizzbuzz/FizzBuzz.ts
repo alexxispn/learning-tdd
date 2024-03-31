8cf1d5ecc9f3c040255bc4496da8906dd82b490a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-03-31 15:08:55 +0100

    green(fake it): convert 6 to 'Fizz'

--- src/1-level/01-fizzbuzz/FizzBuzz.ts
+++ src/1-level/01-fizzbuzz/FizzBuzz.ts
@@ -1,7 +1,9 @@
 export class FizzBuzz {
 
   static convert(integer: number): string {
-    if (integer === 3) return "Fizz"
+    if (integer === 3 || integer === 6) {
+      return "Fizz"
+    }
     return String(integer)
   }
 }
