60ef13008c5d9f9a23b6580be9f7c588ec7d762c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-07 13:54:16 +0100

    red: player two wins

--- src/04-rock-paper-scissors/RockPaperScissors.spec.ts
+++ src/04-rock-paper-scissors/RockPaperScissors.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest";
-import { RockPaperScissors } from "./RockPaperScissors.js";
+import { describe, expect, it } from "vitest"
+import { RockPaperScissors } from "./RockPaperScissors.js"
 
 // playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.SCISSORS => playerOne
 // playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.ROCK => playerOne
@@ -13,32 +13,41 @@ import { RockPaperScissors } from "./RockPaperScissors.js";
 // playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.PAPER => RockPaperScissors.DRAW
 // playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.SCISSORS => RockPaperScissors.DRAW
 
-describe('RockPaperScissors', () => {
+describe("RockPaperScissors", () => {
   it.each([
     [RockPaperScissors.ROCK, RockPaperScissors.SCISSORS],
     [RockPaperScissors.PAPER, RockPaperScissors.ROCK],
-    [RockPaperScissors.SCISSORS, RockPaperScissors.PAPER]
-  ])('wins player one', (playerOneMove, playerTwoMove) => {
+    [RockPaperScissors.SCISSORS, RockPaperScissors.PAPER],
+  ])("wins player one", (playerOneMove, playerTwoMove) => {
     const winner = RockPaperScissors.play(playerOneMove, playerTwoMove)
 
-    expect(winner).toBe('Player one wins!')
+    expect(winner).toBe("Player one wins!")
   })
 
-  it('wins player two', () => {
+  it("wins player two", () => {
     const playerOneMove = RockPaperScissors.ROCK
     const playerTwoMove = RockPaperScissors.PAPER
 
     const winner = RockPaperScissors.play(playerOneMove, playerTwoMove)
 
-    expect(winner).toBe('Player two wins!')
+    expect(winner).toBe("Player two wins!")
   })
 
-  it('wins player two', () => {
+  it("wins player two", () => {
     const playerOneMove = RockPaperScissors.PAPER
     const playerTwoMove = RockPaperScissors.SCISSORS
 
     const winner = RockPaperScissors.play(playerOneMove, playerTwoMove)
 
-    expect(winner).toBe('Player two wins!')
+    expect(winner).toBe("Player two wins!")
+  })
+
+  it("wins player two", () => {
+    const playerOneMove = RockPaperScissors.SCISSORS
+    const playerTwoMove = RockPaperScissors.ROCK
+
+    const winner = RockPaperScissors.play(playerOneMove, playerTwoMove)
+
+    expect(winner).toBe("Player two wins!")
   })
 })
