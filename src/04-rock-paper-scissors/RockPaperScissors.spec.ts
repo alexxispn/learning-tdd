388aa9043d1646216a12128f0bbd6a6de976b426
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-07 12:30:20 +0100

    test: create a list of test cases

--- src/04-rock-paper-scissors/RockPaperScissors.spec.ts
+++ src/04-rock-paper-scissors/RockPaperScissors.spec.ts
@@ -1,5 +1,17 @@
 import { describe, it, expect } from "vitest";
 
+// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.SCISSORS => playerOne
+// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.ROCK => playerOne
+// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.PAPER => playerOne
+//
+// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.PAPER => playerTwo
+// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.SCISSORS => playerTwo
+// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.ROCK => playerTwo
+//
+// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.ROCK => RockPaperScissors.DRAW
+// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.PAPER => RockPaperScissors.DRAW
+// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.SCISSORS => RockPaperScissors.DRAW
+
 describe('RockPaperScissors', () => {
   it('', () => {
   })
