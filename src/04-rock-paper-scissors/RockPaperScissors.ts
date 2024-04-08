e946beb335c79b9d853fa85310156c33d3a94e7b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-08 09:28:54 +0100

    refactor: add type for result

--- src/04-rock-paper-scissors/RockPaperScissors.ts
+++ src/04-rock-paper-scissors/RockPaperScissors.ts
@@ -1,9 +1,11 @@
+type Result = "Player one wins!" | "Player two wins!" | "Draw!"
+
 export class RockPaperScissors {
   static ROCK = "rock"
   static PAPER = "paper"
   static SCISSORS = "scissors"
 
-  static play(playerOneMove: string, playerTwoMove: string) {
+  static play(playerOneMove: string, playerTwoMove: string): Result {
     if (playerOneMove === playerTwoMove) {
       return "Draw!"
     }
