bda6bf9ca82f068b82f44404596e6cd45a9320b6
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-08 09:25:53 +0100

    refactor: remove comments

--- src/04-rock-paper-scissors/RockPaperScissors.spec.ts
+++ src/04-rock-paper-scissors/RockPaperScissors.spec.ts
@@ -1,18 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { RockPaperScissors } from "./RockPaperScissors.js"
 
-// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.SCISSORS => playerOne
-// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.ROCK => playerOne
-// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.PAPER => playerOne
-//
-// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.PAPER => playerTwo
-// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.SCISSORS => playerTwo
-// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.ROCK => playerTwo
-//
-// playerOne = RockPaperScissors.ROCK && playerTwo = RockPaperScissors.ROCK => RockPaperScissors.DRAW
-// playerOne = RockPaperScissors.PAPER && playerTwo = RockPaperScissors.PAPER => RockPaperScissors.DRAW
-// playerOne = RockPaperScissors.SCISSORS && playerTwo = RockPaperScissors.SCISSORS => RockPaperScissors.DRAW
-
 describe("RockPaperScissors", () => {
   it.each([
     [RockPaperScissors.ROCK, RockPaperScissors.SCISSORS],
