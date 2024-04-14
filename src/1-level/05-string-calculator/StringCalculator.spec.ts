b66489700166b0ef07678a0f3cb9e0a98ba83b60
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 18:19:22 +0100

    step1-green: sum 0 for empty string

--- src/1-level/05-string-calculator/StringCalculator.spec.ts
+++ src/1-level/05-string-calculator/StringCalculator.spec.ts
@@ -3,7 +3,9 @@ import { describe, expect, it } from "vitest"
 // ""       ->    0     -> no numbers   ->  0
 // "4"      ->    4     -> one number   ->  the number
 class StringCalculator {
-  add(numbers: string) {}
+  add(numbers: string): number {
+    return 0
+  }
 }
 
 // "1, 2"   ->    3     -> two numbers  ->  the sum
