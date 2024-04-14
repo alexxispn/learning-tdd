409f80e03f4a3b31ddf1f839d2b511d4ed139d19
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 19:08:53 +0100

    step1-refactor: extract method extractNumbers

--- src/1-level/05-string-calculator/StringCalculator.ts
+++ src/1-level/05-string-calculator/StringCalculator.ts
@@ -1,6 +1,10 @@
 export class StringCalculator {
   add(numbers: string): number {
-    const numbersArray: number[] = numbers.split(",").map(Number)
-    return numbersArray.reduce((sum, number) => sum + number, 0)
+    const extractedNumbers = this.extractNumbers(numbers)
+    return extractedNumbers.reduce((sum, number) => sum + number, 0)
+  }
+
+  private extractNumbers(numbers: string) {
+    return numbers.split(",").map(Number)
   }
 }
