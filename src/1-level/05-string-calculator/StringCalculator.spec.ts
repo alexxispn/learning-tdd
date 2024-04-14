4e7a053bcb6dd7e41d6a3026b7c7a7bbd2a8578a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 18:11:36 +0100

    step1-test: add a list of test cases

--- src/1-level/05-string-calculator/StringCalculator.spec.ts
+++ src/1-level/05-string-calculator/StringCalculator.spec.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from "vitest"
 
+// ""       ->    0     -> no numbers   ->  0
+// "4"      ->    4     -> one number   ->  the number
+// "1, 2"   ->    3     -> two numbers  ->  the sum
 describe("StringCalculator", () => {
   it("", () => {})
 })
