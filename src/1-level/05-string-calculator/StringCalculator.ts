e1744892e943714aaa272ca59ba48d5109b68c2a
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 19:21:46 +0100

    step1-green: throws an error when it receives invalid input

--- src/1-level/05-string-calculator/StringCalculator.ts
+++ src/1-level/05-string-calculator/StringCalculator.ts
@@ -1,4 +1,6 @@
 export class StringCalculator {
+  static readonly INVALID_INPUT_ERROR_MESSAGE = "Invalid input"
+
   add(numbers: string): number {
     return this.sumNumbers(this.extractNumbers(numbers))
   }
@@ -8,6 +10,12 @@ export class StringCalculator {
   }
 
   private extractNumbers(numbers: string) {
-    return numbers.split(",").map(Number)
+    return numbers.split(",").map((number) => {
+      const parsedNumber = Number(number)
+      if (isNaN(parsedNumber)) {
+        throw new Error(StringCalculator.INVALID_INPUT_ERROR_MESSAGE)
+      }
+      return parsedNumber
+    })
   }
 }
