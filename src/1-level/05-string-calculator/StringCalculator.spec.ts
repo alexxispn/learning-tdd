1f391770a925926bfd121eb1046bd9c21f8f3f9c
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 19:22:39 +0100

    step1-refactor: remove comments

--- src/1-level/05-string-calculator/StringCalculator.spec.ts
+++ src/1-level/05-string-calculator/StringCalculator.spec.ts
@@ -1,9 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { StringCalculator } from "./StringCalculator.js"
 
-// ""       ->    0     -> no numbers   ->  0
-// "4"      ->    4     -> one number   ->  the number
-// "1, 2"   ->    3     -> two numbers  ->  the sum
 describe("StringCalculator", () => {
   it.each(["", " ", "  ", "   ", "    "])("adds 0 when it receives an empty string", (numbers) => {
     const stringCalculator = new StringCalculator()
