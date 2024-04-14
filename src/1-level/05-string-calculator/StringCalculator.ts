c2b01a964afc3e1517d64dfed50c9265624ce48b
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 20:52:41 +0100

    step4-refactor: extract method extractCustomDelimiter

--- src/1-level/05-string-calculator/StringCalculator.ts
+++ src/1-level/05-string-calculator/StringCalculator.ts
@@ -2,7 +2,8 @@ export class StringCalculator {
   static readonly INVALID_INPUT_ERROR_MESSAGE = "Invalid input"
 
   add(text: string): number {
-    const extractedValues = this.extractValues(text)
+    const customDelimiter = this.extractCustomDelimiter(text)
+    const extractedValues = this.extractValues(text, customDelimiter)
     const numbers = this.parseToNumbers(extractedValues)
     return this.sumNumbers(numbers)
   }
@@ -11,19 +12,20 @@ export class StringCalculator {
     return extractedNumbers.reduce((sum, number) => sum + number, 0)
   }
 
-  private extractValues(text: string): string[] {
-    const defaultDelimiters = [",", "\n"]
-    const hasCustomDelimiter = text.startsWith("//")
-    const delimiters = hasCustomDelimiter ? [text[2], ...defaultDelimiters] : defaultDelimiters
-    const values = hasCustomDelimiter ? text.slice(4) : text
-    const regex = new RegExp(`[${delimiters.join()}]`)
-    return values.split(regex)
-  }
-
   private parseToNumbers(values: string[]): number[] {
     if (values.some((value) => isNaN(Number(value)))) {
       throw new Error(StringCalculator.INVALID_INPUT_ERROR_MESSAGE)
     }
     return values.map(Number)
+  }
+
+  private extractValues(text: string, customDelimiter: string): string[] {
+    const delimiters = [",", "\n", customDelimiter]
+    const delimiterRegex = new RegExp(`[${delimiters.join("")}]`)
+    return customDelimiter ? text.slice(4).split(customDelimiter) : text.split(delimiterRegex)
+  }
+
+  private extractCustomDelimiter(text: string) {
+    return text.startsWith("//") ? text[2] : ""
   }
 }
