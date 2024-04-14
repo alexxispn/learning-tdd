c59d7cfdea847763c27bfcf52a248962652ce9e1
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-04-14 18:18:43 +0100

    step1-red: sum 0 for empty string

--- src/1-level/05-string-calculator/StringCalculator.spec.ts
+++ src/1-level/05-string-calculator/StringCalculator.spec.ts
@@ -2,7 +2,17 @@ import { describe, expect, it } from "vitest"
 
 // ""       ->    0     -> no numbers   ->  0
 // "4"      ->    4     -> one number   ->  the number
+class StringCalculator {
+  add(numbers: string) {}
+}
+
 // "1, 2"   ->    3     -> two numbers  ->  the sum
 describe("StringCalculator", () => {
-  it("", () => {})
+  it("sum 0 for empty string", () => {
+    const stringCalculator = new StringCalculator()
+
+    const sum = stringCalculator.add("")
+
+    expect(sum).toBe(0)
+  })
 })
